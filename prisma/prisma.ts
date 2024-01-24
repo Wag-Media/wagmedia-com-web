6dfa3b9bae50f98361c5082586c90cb76931fe78
Niklas P <contact@niklasplessing.net>
2024-01-24 20:38:52 +0100

    bring back pooling

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,19 +1,17 @@
 import { PrismaClient } from "@prisma/client"
 
-// let prisma: PrismaClient
-// declare global {
-//   var prisma: PrismaClient | undefined
-// }
+let prisma: PrismaClient
+declare global {
+  var prisma: PrismaClient | undefined
+}
 
-// if (process.env.NODE_ENV === "production") {
-//   prisma = new PrismaClient()
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient()
-//   }
-//   prisma = global.prisma
-// }
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
 
-// export default prisma
-
-export const prisma = new PrismaClient()
+export default prisma
