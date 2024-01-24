08d86490cfd13e3f8ac411fd6ceddb6822cc52a2
Niklas P <contact@niklasplessing.net>
2024-01-24 20:51:17 +0100

    wip

---
+++ app/audit/layout.tsx
@@ -0,0 +1,11 @@
+export default function PricingLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="mx-8">{children}</div>
+    </section>
+  )
+}
