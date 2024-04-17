e9d201a0fbf07a52342cb269c45580c78f5a54ce
Niklas P <contact@niklasplessing.net>
2024-04-18 00:47:33 +0200

    wip

--- data/dbAuthors.ts
+++ data/dbAuthors.ts
@@ -1,8 +1,20 @@
 import { prisma } from "@/prisma/prisma"
-import _ from "lodash"
+import { User } from "@prisma/client"
+import { includes } from "lodash"
+
+import { UserWithPosts } from "./types"
+
+export async function getAuthor(name: string) {
+  const author = await prisma.user.findFirst({
+    where: {
+      name,
+    },
+  })
+
+  return author
+}
 
 export async function getAuthors() {
-  console.log("authors called")
   const authors = await prisma.user.findMany({
     where: {
       posts: {
@@ -49,19 +61,32 @@ export async function getAuthors() {
   })
 
   return authors
-    .map((author) => ({
-      ...author,
-      postCount: author.posts.length, // Calculate the number of posts for each author
-      totalEarnings: author.posts.reduce(
-        (sum, post) =>
-          sum +
-          post.earnings.reduce(
-            (postSum, earning) => postSum + earning.totalAmount,
-            0
-          ),
-        0
-      ),
-    }))
+    .map(
+      (
+        author: User & {
+          posts: {
+            id: string
+            title: string
+            earnings: {
+              totalAmount: number
+              unit: string
+            }[]
+          }[]
+        }
+      ) => ({
+        ...author,
+        postCount: author.posts.length, // Calculate the number of posts for each author
+        totalEarnings: author.posts.reduce(
+          (sum, post) =>
+            sum +
+            post.earnings.reduce(
+              (postSum, earning) => postSum + earning.totalAmount,
+              0
+            ),
+          0
+        ),
+      })
+    )
     .sort((a, b) => b.totalEarnings - a.totalEarnings) // Sort authors by total earnings in descending order
     .slice(0, 10) // Get the top 10 authors
 }
