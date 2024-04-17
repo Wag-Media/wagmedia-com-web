c3c714a6e8c878e538512e189a3a9b85a8df3548
Niklas P <contact@niklasplessing.net>
2024-04-17 18:53:14 +0200

    slider and mobile fix

---
+++ components/CardLarge1/CardLarge1Wag.tsx
@@ -0,0 +1,119 @@
+import React, { FC } from "react"
+import Link from "next/link"
+import {
+  PostDataType,
+  PostWithTagsCategoriesReactionsPaymentsUser,
+} from "@/data/types"
+
+import CardAuthor2 from "@/components/CardAuthor2/CardAuthor2"
+import CategoryBadgeList from "@/components/CategoryBadgeList/CategoryBadgeList"
+import NcImage from "@/components/NcImage/NcImage"
+import NextPrev from "@/components/NextPrev/NextPrev"
+import PostCardLikeAndComment from "@/components/PostCardLikeAndComment/PostCardLikeAndComment"
+import PostCardSaveAction from "@/components/PostCardSaveAction/PostCardSaveAction"
+import PostTypeFeaturedIcon from "@/components/PostTypeFeaturedIcon/PostTypeFeaturedIcon"
+
+import CardAuthor2Wag from "../CardAuthor2/CardAuthor2Wag"
+import CategoryBadgeListWag from "../CategoryBadgeList/CategoryBadgeListWag"
+import { WagImage } from "../WagImage/WagImage"
+
+export interface CardLarge1WagProps {
+  className?: string
+  post: PostWithTagsCategoriesReactionsPaymentsUser
+  onClickNext?: () => void
+  onClickPrev?: () => void
+}
+
+const CardLarge1Wag: FC<CardLarge1WagProps> = ({
+  className = "",
+  post,
+  onClickNext = () => {},
+  onClickPrev = () => {},
+}) => {
+  const {
+    id,
+    title,
+    content,
+    slug,
+    tags,
+    categories,
+    reactions,
+    payments,
+    embeds,
+    createdAt,
+    discordLink,
+    user,
+    earnings,
+  } = post
+
+  const firstEmbed = embeds?.[0] ?? null
+  const featuredImage = firstEmbed?.embedImage
+
+  return (
+    <div
+      className={`nc-CardLarge1 nc-CardLarge1--hasAnimation relative flex flex-col-reverse md:flex-row justify-end ${className}`}
+    >
+      <div className="md:absolute z-10 md:start-0 md:top-1/2 md:-translate-y-1/2 w-full -mt-8 md:mt-0 px-3 sm:px-6 md:px-0 md:w-3/5 lg:w-1/2 xl:w-2/5">
+        <div className="nc-CardLarge1__left p-4 sm:p-8 xl:py-14 md:px-10 bg-white/40 dark:bg-neutral-900/40 backdrop-filter backdrop-blur-lg shadow-lg dark:shadow-2xl rounded-lg space-y-3 sm:space-y-5 ">
+          <CategoryBadgeListWag categories={categories} />
+
+          <h2 className="nc-card-title text-base sm:text-xl lg:text-2xl font-semibold ">
+            <Link
+              href={`/post/${post.slug}`}
+              className="line-clamp-2"
+              title={title}
+            >
+              {title}
+            </Link>
+          </h2>
+
+          <CardAuthor2Wag className="relative" author={user} date={createdAt} />
+
+          {/* <div className="flex items-center justify-between mt-auto">
+            <PostCardLikeAndComment />
+            <PostCardSaveAction bookmarkClass="h-8 w-8 bg-neutral-50/30 hover:bg-neutral-50/50 dark:bg-neutral-800/30 dark:hover:bg-neutral-800/50" />
+          </div> */}
+        </div>
+        <div className="p-4 sm:pt-8 sm:px-10">
+          <NextPrev
+            btnClassName="w-11 h-11 text-xl"
+            onClickNext={onClickNext}
+            onClickPrev={onClickPrev}
+          />
+        </div>
+      </div>
+      <div className="w-full md:w-4/5 lg:w-2/3">
+        <Link
+          href={`/post/${post.slug}`}
+          className="nc-CardLarge1__right block relative"
+        >
+          {/* <NcImage
+            containerClassName="aspect-w-16 aspect-h-12 sm:aspect-h-9 md:aspect-h-14 lg:aspect-h-10 2xl:aspect-h-9 relative"
+            className="absolute inset-0 object-cover rounded-lg"
+            src={featuredImage}
+            alt={title}
+            fill
+            sizes="(max-width: 768px) 100vw, 50vw"
+            priority
+          /> */}
+
+          <WagImage
+            image={featuredImage}
+            containerClassName="aspect-w-16 aspect-h-12 sm:aspect-h-9 md:aspect-h-14 lg:aspect-h-10 2xl:aspect-h-9 relative"
+            className="absolute inset-0 object-cover rounded-lg"
+            fill
+            priority
+            alt={title}
+          />
+          {/* META TYPE */}
+          {/* <PostTypeFeaturedIcon
+            postType={post.postType}
+            className="absolute w-8 h-8 md:w-10 md:h-10 right-6 top-6"
+          /> */}
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default CardLarge1Wag
--- components/Header/MainNav2Logged.tsx
+++ components/Header/MainNav2Logged.tsx
@@ -1,39 +1,41 @@
-"use client";
+"use client"
 
-import React, { FC } from "react";
-import Logo from "@/components/Logo/Logo";
-import MenuBar from "@/components/MenuBar/MenuBar";
-import AvatarDropdown from "./AvatarDropdown";
-import SearchModal from "./SearchModal";
-import NotifyDropdown from "./NotifyDropdown";
-import SwitchDarkMode from "../SwitchDarkMode/SwitchDarkMode";
-import { DiscordIcon } from "@/images/icons";
-import NavigationWag from "../Navigation/NavigationWag";
+import React, { FC } from "react"
+import { DiscordIcon } from "@/images/icons"
+
+import Logo from "@/components/Logo/Logo"
+import MenuBar from "@/components/MenuBar/MenuBar"
+
+import NavigationWag from "../Navigation/NavigationWag"
+import SwitchDarkMode from "../SwitchDarkMode/SwitchDarkMode"
+import AvatarDropdown from "./AvatarDropdown"
+import NotifyDropdown from "./NotifyDropdown"
+import SearchModal from "./SearchModal"
 
 export interface MainNav2LoggedProps {}
 
 const MainNav2Logged: FC<MainNav2LoggedProps> = () => {
   const renderContent = () => {
     return (
-      <div className="h-20 flex justify-between">
-        <div className="flex items-center lg:hidden flex-1">
+      <div className="flex h-20 justify-between">
+        <div className="flex items-center lg:hidden ">
           <MenuBar />
         </div>
 
-        <div className="lg:flex-1 flex items-center">
+        <div className="flex flex-1 items-center">
           <Logo />
         </div>
 
-        <div className="flex-[2] hidden lg:flex justify-center mx-4">
+        <div className="mx-4 hidden flex-[2] justify-center lg:flex">
           <NavigationWag />
         </div>
 
-        <div className="flex-1 flex items-center justify-end text-slate-700 dark:text-slate-100">
-          {/* <SearchModal /> */}
+        <div className="flex flex-1 items-center justify-end text-slate-700 dark:text-slate-100">
+          <SearchModal />
           {/* <NotifyDropdown /> */}
           <SwitchDarkMode />
           <button
-            className={`self-center text-2xl md:text-3xl w-12 h-12 rounded-full text-neutral-500 dark:text-neutral-300 hover:bg-neutral-100 dark:hover:bg-neutral-800 focus:outline-none flex items-center justify-center`}
+            className={`flex h-12 w-12 items-center justify-center self-center rounded-full text-2xl text-neutral-500 hover:bg-neutral-100 focus:outline-none dark:text-neutral-300 dark:hover:bg-neutral-800 md:text-3xl`}
           >
             <span className="sr-only">Enable dark mode</span>
             <DiscordIcon size={38} />
@@ -42,14 +44,14 @@ const MainNav2Logged: FC<MainNav2LoggedProps> = () => {
           {/* <AvatarDropdown /> */}
         </div>
       </div>
-    );
-  };
+    )
+  }
 
   return (
-    <div className="nc-MainNav2Logged relative z-10 bg-white/50 dark:bg-neutral-900/50 backdrop-blur-md">
+    <div className="nc-MainNav2Logged relative z-10 bg-white/80 dark:bg-neutral-900/50 lg:bg-white/50 lg:backdrop-blur-md">
       <div className="container ">{renderContent()}</div>
     </div>
-  );
-};
+  )
+}
 
-export default MainNav2Logged;
+export default MainNav2Logged
