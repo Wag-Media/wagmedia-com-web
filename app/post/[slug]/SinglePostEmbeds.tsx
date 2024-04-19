1db0198ad43f9924d04f3ab00249582964cf6d6c
Niklas P <contact@niklasplessing.net>
2024-04-19 11:14:00 +0200

    single post page

---
+++ app/post/[slug]/SinglePostEmbeds.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { Embed } from "@prisma/client"
+import { InstagramEmbed, YouTubeEmbed } from "react-social-media-embed"
+import { Tweet } from "react-tweet"
+
+import { getEmbedType } from "./util"
+
+export function SinglePostEmbeds({ embeds }: { embeds: Embed[] }) {
+  return (
+    <div className="nc-SinglePostEmbeds overflow-hidden my-8">
+      {/* <>{JSON.stringify(embeds, null, 2)}</> */}
+      {embeds.map((embed, index) => {
+        const embedType = getEmbedType(embed.embedUrl)
+
+        if (embedType === "twitter") {
+          const tweetId = embed.embedUrl?.split("/").pop()
+
+          return (
+            <div key={index} className="nc-SinglePostEmbeds__twitter">
+              {tweetId && <Tweet id={tweetId} />}
+            </div>
+          )
+        }
+
+        if (embedType === "youtube" && embed.embedUrl) {
+          return (
+            <div
+              key={index}
+              className="nc-SinglePostEmbeds__youtube rounded-xl aspect-w-16 aspect-h-9 overflow-hidden"
+            >
+              <YouTubeEmbed
+                url={embed.embedUrl}
+                width="100%"
+                height="100%"
+                style={{ aspectRatio: "16/9" }}
+              />
+            </div>
+          )
+        }
+
+        if (embedType === "instagram" && embed.embedUrl) {
+          return (
+            <div
+              key={index}
+              className="nc-SinglePostEmbeds__instagram overflow-hidden w-full"
+            >
+              <InstagramEmbed
+                url={embed.embedUrl}
+                width="100%"
+                style={{ maxWidth: "600px" }}
+              />
+            </div>
+          )
+        }
+
+        return null
+      })}
+    </div>
+  )
+}
