1a117690124f58beda1f15e1314fa84a6867266b
Niklas P <contact@niklasplessing.net>
2024-04-25 15:24:38 +0200

    wip

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react"
+import Link from "next/link"
 import { CustomLink } from "@/data/types"
 
+import { siteConfig } from "@/config/site"
 import Logo from "@/components/Logo/Logo"
 import SocialsList1 from "@/components/SocialsList1/SocialsList1"
 
@@ -87,17 +89,27 @@ const Footer: React.FC = () => {
       {/* <MusicPlayer /> */}
 
       {/* footer */}
-      <div className="nc-Footer relative py-16 lg:py-28 border-t border-neutral-200 dark:border-neutral-700">
-        <div className="container grid grid-cols-2 gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-10 ">
-          <div className="grid grid-cols-4 gap-5 col-span-2 md:col-span-4 lg:md:col-span-1 lg:flex lg:flex-col">
-            <div className="col-span-2 md:col-span-1">
-              <Logo />
-            </div>
-            <div className="col-span-2 flex items-center md:col-span-3">
-              <SocialsList1 className="flex items-center space-x-3 lg:space-x-0 rtl:space-x-reverse lg:flex-col lg:space-y-2.5 lg:items-start" />
-            </div>
+      <div className="nc-Footer relative py-8 lg:py-16 border-t border-neutral-200 dark:border-neutral-700">
+        <div className="container grid grid-cols-2 gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-10 items-center">
+          <div className="col-span-1 md:col-span-1">
+            <Logo />
           </div>
-          {widgetMenus.map(renderWidgetMenuItem)}
+          <div className="col-span-3 md:col-span-3">
+            <p className="text-neutral-500 dark:text-neutral-400 max-w-md">
+              WagMedia is a decentralized media platform that allows creators to
+              earn cryptocurrency for their content.
+            </p>
+          </div>
+          <div className="col-span-1 md:col-span-1">
+            <nav className="flex space-x-3 text-md text-neutral-6000 dark:text-neutral-300 flex items-center space-x-3 lg:space-x-0 rtl:space-x-reverse lg:flex-col lg:space-y-2.5 lg:items-end">
+              {siteConfig.navMenuItems.map((item, index) => (
+                <Link key={index} href={item.href}>
+                  {item.name}
+                </Link>
+              ))}
+            </nav>
+          </div>
+          {/* {widgetMenus.map(renderWidgetMenuItem)} */}
         </div>
       </div>
     </>
--- components/Navigation/NavigationWag.tsx
+++ components/Navigation/NavigationWag.tsx
@@ -2,44 +2,12 @@ import React, { FC, use } from "react"
 import { usePathname, useRouter } from "next/navigation"
 import { NAVIGATION_DEMO_2 } from "@/data/navigation"
 
+import { siteConfig } from "../../config/site"
 import NavigationItem, { NavItemType } from "./NavigationItem"
 
 interface Props {
   className?: string
 }
-
-const navMenuItems: NavItemType[] = [
-  {
-    id: "1",
-    name: "Categories",
-    href: "/categories",
-  },
-  {
-    id: "1",
-    name: "Non Anglo",
-    href: "/non-anglo",
-  },
-  {
-    id: "2",
-    name: "Newsletter",
-    href: "/newsletter",
-  },
-  {
-    id: "3",
-    name: "News",
-    href: "/news",
-  },
-  {
-    id: "4",
-    name: "Audit",
-    href: "/audit",
-  },
-  {
-    id: "5",
-    name: "About",
-    href: "/about",
-  },
-]
 
 const NavigationWag: FC<Props> = ({ className = "flex" }) => {
   const router = useRouter()
@@ -49,7 +17,7 @@ const NavigationWag: FC<Props> = ({ className = "flex" }) => {
     <ul
       className={`nc-Navigation rounded-full bg-background/60 backdrop-blur-md items-center flex ${className}`}
     >
-      {navMenuItems.map((item) => (
+      {siteConfig.navMenuItems.map((item) => (
         <NavigationItem
           key={item.id}
           menuItem={item}
