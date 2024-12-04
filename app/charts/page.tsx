913d8a92a7ab3ccf14e5f7ac0ff7c37f3c4b3b6f
Niklas P <contact@niklasplessing.net>
2024-12-04 13:59:57 +0100

    bring utils back

--- app/charts/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuditGraphs } from "@/components/ui/audit/graphs/oddjob/oddjob-by-roles"
-
-export default function PageCharts() {
-  return (
-    <div>
-      <AuditGraphs />
-    </div>
-  )
-}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,36 @@
+import { PostWithReactions } from "@/data/types"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function postHasFlag(post: PostWithReactions) {
+  // Check if the post has a flag emoji in its reactions
+  return post.reactions.some((reaction) => isCountryFlag(reaction.emoji.id))
+}
+
+export function getPostFlag(post: PostWithReactions) {
+  // Get the flag emoji from the post reactions
+  const flagReaction = post.reactions.find((reaction) =>
+    isCountryFlag(reaction.emoji.id)
+  )
+
+  return flagReaction?.emoji.id || null
+}
+
+/**
+ * Checks if the given emoji is a flag emoji
+ * @param emoji
+ * @returns
+ */
+export function isCountryFlag(emoji: string | null): boolean {
+  if (!emoji) return false
+
+  // Regular expression to match exactly one flag emoji
+  // The ^ and $ anchors ensure that the entire string is just one flag emoji
+  const flagRegex = /^\p{Regional_Indicator}{2}$/u
+
+  return flagRegex.test(emoji)
 }
