e2feabaf4e8622b1b5edb0bd19928645b1ab2adc
Niklas P <contact@niklasplessing.net>
2025-01-15 12:25:21 +0100

    try remove prisma pooling

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,25 +1,3 @@
 import { PrismaClient } from "@prisma/client"
-import { PrismaClientOptions } from "@prisma/client/runtime/library"
-import { withAccelerate } from "@prisma/extension-accelerate"
 
-let prisma: PrismaClient
-declare global {
-  var prisma: PrismaClient | undefined
-}
-
-if (process.env.NODE_ENV === "production") {
-  if (process.env.DATABASE_URL?.includes("accelerate")) {
-    prisma = new PrismaClient().$extends(
-      withAccelerate()
-    ) as unknown as PrismaClient
-  } else {
-    prisma = new PrismaClient()
-  }
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
-
-export { prisma }
+export const prisma = new PrismaClient()
