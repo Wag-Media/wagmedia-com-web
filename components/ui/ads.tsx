b51bde2c03a47aade83db4339447beb094d99051
Niklas P <contact@niklasplessing.net>
2025-01-21 10:57:09 +0100

    remove 50 unused files

--- components/ui/ads.tsx
+++
@@ -1,7 +0,0 @@
-export function Ads() {
-  return (
-    <div className="flex h-44 w-full items-center justify-center bg-blue-300">
-      <h1 className="text-xs">Ads</h1>
-    </div>
-  )
-}
--- hooks/useNcId.ts
+++
@@ -1,6 +0,0 @@
-import { useId } from "react";
-
-export default function useNcId(pre = "nc"): string {
-  const id = useId();
-  return `${pre}_${id.replace(/:/g, "_")}`;
-}
