c1e7664e1d1a461a1e062a24ad40fb8604b10808
Niklas P <contact@niklasplessing.net>
2025-01-31 13:11:42 +0100

    remove prisma accelerate

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,6 +1,4 @@
 import { PrismaClient } from "@prisma/client"
-import { PrismaClientOptions } from "@prisma/client/runtime/library"
-import { withAccelerate } from "@prisma/extension-accelerate"
 
 let prisma: PrismaClient
 declare global {
@@ -8,13 +6,7 @@ declare global {
 }
 
 if (process.env.NODE_ENV === "production") {
-  if (process.env.DATABASE_URL?.includes("accelerate")) {
-    prisma = new PrismaClient().$extends(
-      withAccelerate()
-    ) as unknown as PrismaClient
-  } else {
-    prisma = new PrismaClient()
-  }
+  prisma = new PrismaClient()
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient()
