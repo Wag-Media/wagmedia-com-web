d0c27bb7b4e170288ac74e03284abad8057968d0
Niklas P <contact@niklasplessing.net>
2025-02-01 16:13:08 +0100

    wip

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -7,6 +7,8 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
+console.log("process.env.NODE_ENV", process.env.NODE_ENV)
+
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient()
 } else {
